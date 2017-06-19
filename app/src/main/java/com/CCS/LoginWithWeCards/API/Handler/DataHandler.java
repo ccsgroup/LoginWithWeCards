0dccc6e746009c674914d8fea1b8953c395fb4a0
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-06-19 12:53:20 +0530

    new change

--- app/src/main/java/com/CCS/LoginWithWeCards/API/Handler/DataHandler.java
+++
@@ -1,9 +0,0 @@
-package com.CCS.LoginWithWeCards.API.Handler;
-
-/**
- * Created by mauliksantoki on 26/4/17.
- */
-
-public interface DataHandler {
-    public void getData(String data, String message);
-}
